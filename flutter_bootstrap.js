25a5a9dca665f7b629e3e1c28ac06efa92b4555b
Marcel-Fe <marcelfehse22@gmx.de>
2026-05-28 11:19:58 +0200

    fix(ki): GET-Endpoint statt POST - umgeht CORS-Preflight + credentials-Issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1527323429" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3419571743" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
