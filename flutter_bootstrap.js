6a88d56d5670821a6f7c5650c39e5a46bbec2e7b
Marcel-Fe <marcelfehse22@gmx.de>
2026-06-02 13:09:51 +0200

    Deploy: Phase 6 (Quiz, Portionsrechner, Kommandos)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3057563362" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1611504591" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
