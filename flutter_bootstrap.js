7fabbcd0385b29507efa934692feaadd3fd0a80f
Marcel-Fe <marcelfehse22@gmx.de>
2026-06-02 11:18:01 +0200

    Deploy: Phase 2 (Dashboard-KI-Leiste, Verhaltenscheck-Freitext)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2506104707" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3794006084" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
